087ae5fa5eb2f6578cab1a9f68105f4e758567b0
Armastus <david.o.bustillos@gmail.com>
2015-05-22 23:16:26 -0700

    Updated SmallestMultiple.  Trying to simplify the code.

--- ProjectEuler/src/SumSquareDifference/SumSquareDifference.java
+++ ProjectEuler/src/SumSquareDifference/SumSquareDifference.java
@@ -1,16 +1,27 @@
 package SumSquareDifference;
 
 /**
- * Created by David on 4/19/2015.
+ * Created by David on 5/19/2015.
+ * The sum of the squares of the first ten natural numbers is,
+ 1^2 + 2^2 + ... + 10^2 = 385
  */
 public class SumSquareDifference {
-    public static void main(String[] args) {
+    public static void main (String[] args){
+        /*int num = 2;
+        System.out.println(Math.pow(2, num));*/
         int total = 0;
-
-        for(int x = 1; x <= 10; x++){
-            for (int y = 1; x <= 10; y++)
-            total = x * 2;
-            System.out.println(total);
+        int sum = 0;
+        for(int x = 1; x <= 100; x++){
+            total += x;
         }
+
+        for(int y = 1; y <= 100; y++){
+            sum += Math.pow(y, 2);
+        }
+
+        System.out.println(Math.pow(total, 2)); // = 3025
+        System.out.println(sum); // = 385
+        //System.out.println(total); = 55
+        System.out.println(Math.pow(total, 2) - sum); //3025 - 385
     }
 }
